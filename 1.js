3f5a02d835b4fceb5e99565b79b4b837eb6ce4af
Ahmed <heydude08080@gmail.com>
2023-09-21 14:02:00 -0700

    Used Promises

--- 1.js
+++ 1.js
@@ -5,30 +5,59 @@ let stocks = {
     container: [`cone`, `cup`],
     toppings: [`chocolate`, `sprinkes`],
 };
-let order = (toppings, container, fruit_name, call_production) => {
-    setTimeout(() => {
-        console.log(`${stocks.fruits[fruit_name]} is selected`);
-        setTimeout(() => {
-            console.log(`${stocks.container[container]} is selected`);
+// let order = (toppings: number, container: number, fruit_name: number, call_production: any) => {
+//     setTimeout(() => {
+//         console.log(`${stocks.fruits[fruit_name]} is selected`);
+//         setTimeout(() => {
+//             console.log(`${stocks.container[container]} is selected`);
+//             setTimeout(() => {
+//                 console.log(`${stocks.toppings[toppings]} is selected as toppings`);
+//             }, 2000);
+//         }, 3000);
+//     }, 2000);
+//     call_production();
+// }
+// let production = () => {
+//     setTimeout(() => {
+//         console.log(`Order has placed`);
+//         setTimeout(() => {
+//             console.log(`Production has started`);
+//             setTimeout(() => {
+//                 console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} is being added`);
+//                 setTimeout(() => {
+//                     console.log(`your Ice cream is ready! Here you go`);
+//                 }, 6000);
+//             }, 2000);
+//         }, 0);
+//     }, 1000)
+// }
+// order(0, 0, 2, production);
+let is_shop_open = true;
+let order = (time, work) => {
+    return new Promise((resolve, reject) => {
+        if (is_shop_open) {
             setTimeout(() => {
-                console.log(`${stocks.toppings[toppings]} is selected as toppings`);
-            }, 2000);
-        }, 3000);
-    }, 2000);
-    call_production();
+                resolve(work());
+            }, time);
+        }
+        else {
+            reject(console.log(`We are sorry shop is closed`));
+        }
+    });
 };
-let production = () => {
-    setTimeout(() => {
-        console.log(`Order has placed`);
-        setTimeout(() => {
-            console.log(`Production has started`);
-            setTimeout(() => {
-                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} is being added`);
-                setTimeout(() => {
-                    console.log(`your Ice cream is ready! Here you go`);
-                }, 6000);
-            }, 2000);
-        }, 0);
-    }, 1000);
-};
-order(0, 0, 2, production);
+order(4000, () => console.log(`${stocks.fruits[0]}`))
+    .then(() => {
+    return order(0, () => console.log(`production has been started`));
+})
+    .then(() => {
+    return order(1000, () => console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} is being added`));
+})
+    .then(() => {
+    return order(1000, () => console.log(`${stocks.container[0]} is selected`));
+})
+    .then(() => {
+    return order(1000, () => console.log(`${stocks.toppings[0]} is being added as topping`));
+})
+    .then(() => {
+    return order(2000, () => console.log(`Enjoy your ice cream!`));
+});
--- 1.ts
+++ 1.ts
@@ -5,38 +5,81 @@ let stocks = {
     toppings: [`chocolate`, `sprinkes`],
 }
 
-let order = (toppings: number, container: number, fruit_name: number, call_production: any) => {
+// let order = (toppings: number, container: number, fruit_name: number, call_production: any) => {
 
-    setTimeout(() => {
-        console.log(`${stocks.fruits[fruit_name]} is selected`);
-        setTimeout(() => {
-            console.log(`${stocks.container[container]} is selected`);
+//     setTimeout(() => {
+//         console.log(`${stocks.fruits[fruit_name]} is selected`);
+//         setTimeout(() => {
+//             console.log(`${stocks.container[container]} is selected`);
+//             setTimeout(() => {
+//                 console.log(`${stocks.toppings[toppings]} is selected as toppings`);
+
+//             }, 2000);
+
+//         }, 3000);
+//     }, 2000);
+
+//     call_production();
+// }
+// let production = () => {
+//     setTimeout(() => {
+//         console.log(`Order has placed`);
+//         setTimeout(() => {
+//             console.log(`Production has started`);
+//             setTimeout(() => {
+//                 console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} is being added`);
+//                 setTimeout(() => {
+//                     console.log(`your Ice cream is ready! Here you go`);
+
+//                 }, 6000);
+
+//             }, 2000);
+
+//         }, 0);
+//     }, 1000)
+// }
+
+// order(0, 0, 2, production);
+
+
+let is_shop_open = true;
+let order = (time: number, work: any) => {
+    return new Promise((resolve, reject) => {
+        if (is_shop_open) {
             setTimeout(() => {
-                console.log(`${stocks.toppings[toppings]} is selected as toppings`);
+                resolve(work())
+            }, time);
 
-            }, 2000);
+        }
+        else {
+            reject(console.log(`We are sorry shop is closed`));
 
-        }, 3000);
-    }, 2000);
+        }
+    })
 
-    call_production();
 }
-let production = () => {
-    setTimeout(() => {
-        console.log(`Order has placed`);
-        setTimeout(() => {
-            console.log(`Production has started`);
-            setTimeout(() => {
-                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} is being added`);
-                setTimeout(() => {
-                    console.log(`your Ice cream is ready! Here you go`);
+order(4000, () => console.log(`${stocks.fruits[0]}`))
 
-                }, 6000);
+    .then(() => {
+        return order(0, () => console.log(`production has been started`));
+    }
+    )
 
-            }, 2000);
+    .then(() => {
+        return order(1000, () => console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} is being added`));
 
-        }, 0);
-    }, 1000)
-}
+    })
+    .then(() => {
+        return order(1000, () => console.log(`${stocks.container[0]} is selected`));
 
-order(0, 0, 2, production);
+    })
+
+    .then(() => {
+        return order(1000, () => console.log(`${stocks.toppings[0]} is being added as topping`));
+
+    })
+
+    .then(() => {
+        return order(2000, () => console.log(`Enjoy your ice cream!`));
+
+    })
