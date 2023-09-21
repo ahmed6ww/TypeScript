4e3bc65467e8fd1094d09540db0b667d9013f578
Ahmed <heydude08080@gmail.com>
2023-09-20 18:53:15 -0700

    Used Callbacks

---
+++ 1.ts
@@ -0,0 +1,42 @@
+let stocks = {
+    fruits: [`Banana`, `Apfel`, `Mangoe`],
+    liquid: [`water`, `ice`],
+    container: [`cone`, `cup`],
+    toppings: [`chocolate`, `sprinkes`],
+}
+
+let order = (toppings: number, container: number, fruit_name: number, call_production: any) => {
+
+    setTimeout(() => {
+        console.log(`${stocks.fruits[fruit_name]} is selected`);
+        setTimeout(() => {
+            console.log(`${stocks.container[container]} is selected`);
+            setTimeout(() => {
+                console.log(`${stocks.toppings[toppings]} is selected as toppings`);
+
+            }, 2000);
+
+        }, 3000);
+    }, 2000);
+
+    call_production();
+}
+let production = () => {
+    setTimeout(() => {
+        console.log(`Order has placed`);
+        setTimeout(() => {
+            console.log(`Production has started`);
+            setTimeout(() => {
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} is being added`);
+                setTimeout(() => {
+                    console.log(`your Ice cream is ready! Here you go`);
+
+                }, 6000);
+
+            }, 2000);
+
+        }, 0);
+    }, 1000)
+}
+
+order(0, 0, 2, production);
